2786e44c20bfcc06343a1f63492b01d3d22f1046
Nico <nico.huebner@protonmail.com>
2023-05-12 15:59:40 +0200

    commented code

--- src/socketConnect.py
+++ src/socketConnect.py
@@ -2,19 +2,15 @@ import socketio
 import requests
 import register_login_User
 
-sio = socketio.Client() 
+## Creates a new instance of the SocketIO client
+sio = socketio.Client()
 
+## Attempts to connect to the SocketIO server with the specified access token
+# @param access_token The access token used to authenticate with the server
 def connect_to_socketio_server(access_token):
     try:
         # Connect to SocketIO server with access token
-        
-        # headers = {'Authorization': 'Bearer ' + access_token}
         sio.connect("https://nope-server.azurewebsites.net", namespaces="/", auth={"token": access_token})
-        # sio.connect('https://nope-server.azurewebsites.net', headers={'Authorization': 'Bearer ' + access_token})
-
-        
-        # Wait for events to be emitted and received
-        # sio.wait()
     except requests.exceptions.RequestException as e:
         print(f'Failed to connect to SocketIO server: {e}')
     except socketio.exceptions.ConnectionError as e:
@@ -22,45 +18,48 @@ def connect_to_socketio_server(access_token):
     except socketio.exceptions.BadNamespaceError as e:
         print(f'Failed to connect to SocketIO server: {e}')
 
-@sio.event
-def connect():
-    print("connected")
-
-@sio.event
-def disconnect():
-    print("disconected")
-
-@sio.event
-def callback(data):
-    print(data)
-
-@sio.on('list:tournaments')
-def socket_on(list, namespace):
-    tournament_list(list)
-# Define the callback function
-
-
-# Function to create a tournament
+## Creates a new tournament with the specified number of games
+# @param num_games The number of games to be included in the tournament
 def create_tournament(num_games):
-    response = sio.call("tournament:create",num_games)
-    print(response)
+    try:
+        response = sio.call("tournament:create",num_games)
+        callback(response)
+    except Exception as e:
+        print(f"Failed to create tournament: {e}")
 
-# Function to join a tournament
+## Joins a tournament with the specified ID
+# @param tournament_id The ID of the tournament to join
 def join_tournament(tournament_id): 
-    response= sio.call("tournament:join", tournament_id )
-    print(response)
+    try:
+        response= sio.call("tournament:join", tournament_id )
+        callback(response)
+    except socketio.exceptions.TimeoutError as e:
+        print(f'Timeout error occurred: {e}')
+    except socketio.exceptions.BadNamespaceError as e:
+        print(f'Bad namespace error occurred: {e}')
 
-def start_tournamet():
-    response= sio.call("tournament:start")
-    print(response)
+## Starts a tournament with the specified ID
+# @param tournament_id The ID of the tournament to start
+def start_tournamet(tournament_id):
+    try:
+        response = sio.call("tournament:start", tournament_id)
+        
+    except Exception as e:
+        print(f"Failed to start tournament: {e}")
 
-def leave_tournament():
-    response = sio.call("tournament:leave")
-    print(response)
+## Leaves a tournament with the specified ID
+# @param tournament_id The ID of the tournament to leave
+def leave_tournament(tournament_id):
+    try:
+        response = sio.call("tournament:leave", tournament_id)
+        
+    except Exception as e:
+        print(f"Failed to leave tournament: {e}")
 
-
+## Formats and prints a list of tournaments received from the server
+# @param data The data received from the server, containing a list of tournaments
+# @return The list of tournaments as a dictionary
 def tournament_list(data):
-
     # Create an empty dictionary to store the tournament data
     tournaments = {}
 
@@ -75,23 +74,36 @@ def tournament_list(data):
         print(f"Status: {tournament_details['status']}")
         print(f"Players: {players}")
         print("")
+    return tournaments
 
-    
+## Event handler for the SocketIO 'connect' event
+@sio.event
+def connect():
+    print("connected")
 
+## Event handler for the SocketIO 'disconnect' event
+@sio.event
+def disconnect():
+    print("disconected")
+
+## Event handler for a callback received from the server
+# @param data The data received from the server
+def callback(data):
+    print(data)
+
+@sio.on('list:tournaments')
+def socket_on(list, namespace):
+    tournaments = tournament_list(list)
 
 def main():
     user = {"username": "nico", "password": "654321"}
     result = register_login_User.login(user)
 
     connect_to_socketio_server(result)
+
+    leave_tournament("clhkk3uhq0004p907sv4cgv1p")
+
     
-    #create_tournament(3)
-    #start_tournamet()
-    join_tournament("clhkk20pa0002p907i4uxdca0")
-    leave_tournament()
-    #create_tournament(3)
-
-
 
 main()
 
