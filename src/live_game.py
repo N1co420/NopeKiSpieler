df8fff7470d594dff8ec0a095f2b04d2b52ebd42
Nico <nico.huebner@protonmail.com>
2023-06-05 15:48:19 +0200

    restructured play_game

--- src/live_game.py
+++ src/live_game.py
@@ -1,6 +1,7 @@
 import socketio
 import payload
 import game_logic
+from events import *
 
 
 def handle_game_status(data):
@@ -11,28 +12,18 @@ def handle_game_status(data):
     top_card = data['topCard']
     last_top_card = data['lastTopCard']
 
+    return current_player_id, hand, top_card, last_top_card
+
 def play_game(jwt_token, player_id):
-    # Authenticate with the server and retrieve the initial game status using the provided token and player ID
-    initial_game_status = get_initial_game_status(jwt_token, player_id)
 
-    # Handle the initial game status
-    handle_game_status(initial_game_status)
+    while (True):
+        sio.on("game:state", game_status)
 
-    # Game loop
-    while not game_over:
-        # Check if it's the client's turn
+        current_player_id, hand, top_card, last_top_card = handle_game_status(game_status)
+
         if game_logic.myTurn(player_id, current_player_id):
-            # Determine the move to be made using game logic
-            move = game_logic.makeMove(hand, top_card, last_top_card)
-
-            # Build the move payload
-            move_payload = payload.buildPayload(move.type, move.cards, move.select_color)
-
-            # Send the move payload to the server
-            payload.send_payload(move_payload)
-
-        # Wait for the next game status update from the server
-        new_game_status = wait_for_game_status_update()
-
-        # Handle the new game status
-        handle_game_status(new_game_status)
+            
+            cards = game_logic.makeMove(hand, top_card, last_top_card)
+            
+            # build payload
+            
