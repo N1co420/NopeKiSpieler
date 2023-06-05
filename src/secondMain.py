abf2baf4d317e21c55323260f754bfd1da46a236
Nico <nico.huebner@protonmail.com>
2023-06-05 14:45:08 +0200

    added 2 mains for testing

---
+++ src/secondMain.py
@@ -0,0 +1,43 @@
+import socketio
+import requests
+import register_login_User
+import socketConnect
+from events import *
+
+def main():
+    # Connect to the server
+    user = {"username": "nico2", "password": "654321"}
+    result, id = register_login_User.login(user)
+
+    socketConnect.connect_to_socketio_server(result)
+
+    while True:
+        # Display menu options
+        print("Menu:")
+        print("q. Create Tournament")
+        print("w. Join Tournament")
+        print("e. Start Tournament")
+        print("r. Leave Tournament")
+        print("t. Disconnect")
+
+        # Get user input
+        choice = input("Enter your choice: ")
+
+        if choice == "q":
+            num_games = int(input("Enter the number of games: "))
+            socketConnect.create_tournament(num_games)
+        elif choice == "w":
+            tournament_id = input("Enter the tournament ID: ")
+            socketConnect.join_tournament(tournament_id)
+        elif choice == "e":
+            socketConnect.start_tournament()
+        elif choice == "r":
+            socketConnect.leave_tournament()
+        elif choice == "t":
+            socketConnect.disconnect_from_socketio_server()
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+
+main()
