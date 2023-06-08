e872d794ee9c8a08b60ee607e58be59f33ecb3b4
Nico <nico.huebner@protonmail.com>
2023-06-08 22:00:44 +0200

    gamechanges

--- src/live_game.py
+++ src/live_game.py
@@ -1,29 +1,30 @@
 import socketio
 import payload
 import game_logic
-from events import *
+from events import sio, game_status
 
 
-def handle_game_status(data):
+def handle_game_status():
     # Extract necessary information from the game status data
-    player_id = data['playerID']
-    current_player_id = data['currentPlayerID']
-    hand = data['hand']
-    top_card = data['topCard']
-    last_top_card = data['lastTopCard']
+    player_id = game_status['playerID']
+    current_player_id = game_status['currentPlayerID']
+    hand = game_status['hand']
+    top_card = game_status['topCard']
+    last_top_card = game_status['lastTopCard']
 
     return current_player_id, hand, top_card, last_top_card
 
 def play_game(jwt_token, player_id):
 
     while (True):
-        sio.on("game:state", game_status)
 
-        current_player_id, hand, top_card, last_top_card = handle_game_status(game_status)
+        current_player_id, hand, top_card, last_top_card = handle_game_status()
 
         if game_logic.myTurn(player_id, current_player_id):
             
-            cards = game_logic.makeMove(hand, top_card, last_top_card)
+            print("Its My Turn!")
+            move_payload = payload.buildPayload("take")
             
+            payload.send_payload(move_payload)
             # build payload
             
--- src/payload.py
+++ src/payload.py
@@ -1,49 +1,41 @@
 import socketConnect
 from events import *
 
-def buildPayload(move_type, cards, select_color):
+def buildPayload(move_type, cards, reason):
     payload = {
         "type": move_type,
         "card1": None,
         "card2": None,
-        "card3": None
+        "card3": None,
+        "reason" : "A"
     }
     
     if move_type == "put":
         payload["type"] = "put"
         for i in range(len(cards)):
-            card_payload = create_number_card_payload(cards[i])
+            card_payload = create_card_payload(cards[i])
             payload[f"card{i + 1}"] = card_payload
     elif move_type == "take":
         payload["type"] = "take"
     elif move_type == "nope":
         payload["type"] = "nope"
-    elif move_type == "selection":
-        if cards and len(cards) > 0:
-            payload["card1"] = create_selection_card_payload(cards[0], select_color)
+
+    payload["reason"] = reason
+    
+    return payload
 
     
-def create_number_card_payload(card):
-    return {
-        "type": "number",
-        "color": card["color"],
-        "value": card["value"]
-    }
+def create_card_payload(card):
+    if card["type"] == "number":
+        return {
+            "type": card["type"],
+            "color": card["color"],
+            "value": card["value"]
+        }
+    
 
-def create_selection_card_payload(card, select_color):
-    return {
-        "type": "selection",
-        "color": "multi",
-        "selectValue": 1,
-        "selectColor": select_color
-    }
 
-def send_payload(payload):
+# Do stuff for action cards
 
-    response = sio.emit("game:makeMove",payload)
-    # Check response status code
-    if response.status_code == 200:
-        print("Payload sent successfully!\n")
-    else:
-        print("Failed to send payload. Error:\n", response.text)
+
 
