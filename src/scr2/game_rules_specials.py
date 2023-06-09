10f615557de66bdf99d1be4b06700928492991df
Nico <nico.huebner@protonmail.com>
2023-06-09 11:31:35 +0200

    added joker functionality

--- src/scr2/game_rules_specials.py
+++ src/scr2/game_rules_specials.py
@@ -1,3 +1,5 @@
+import game_rules_numbercards
+from itertools import combinations
 
 def cardOfChoice(hand):
     print("get Card of choice")
@@ -6,6 +8,19 @@ def cardOfChoice(hand):
 def seeThough():
     print("TopCard = LastTopCard")
     print("Call KI again")
+
+def doubleSeeThrough():
+    print("use 2 Cards down")
+
+def getJokers(hand):
+    jokers = []
+
+    for card in hand:
+        cardType = card["type"]
+        if cardType == "joker":
+            jokers.append(card)
+
+    return jokers
 
 def matchingColor(cardColor, topCardColor):
     cardColors = cardColor.split('-')
@@ -17,5 +32,44 @@ def matchingColor(cardColor, topCardColor):
 
     return False
 
+def getMatchingCards(hand, topCard):
+    jokers = getJokers(hand)
+
+    matchingCards = game_rules_numbercards.get_matching_cards(hand, topCard)
+
+    matchingCards.extend(jokers)
+
+    return matchingCards
+
 def get_possible_sets(matchingCards, topCard):
-    print("mit Joker, See-Through and restart")
+    
+    matchingSets = game_rules_numbercards.get_possible_number_sets(matchingCards, topCard)
+
+    if matchingSets == None:
+        matchingJokerSets = jokerCanFillSet(matchingCards, topCard)
+        return matchingJokerSets
+    else:
+        return matchingSets
+
+def jokerCanFillSet(matchingCards, topCard):
+    setSize = topCard["value"]
+    possibleSets = []
+
+    for combination in combinations(matchingCards, setSize):
+        setsWithJoker = all((card["type"] == "number" or card["type"] == "joker") for card in combination)
+        if setsWithJoker:
+            possibleSets.append(list(combination))
+
+    if len(possibleSets) > 0:
+        return possibleSets
+    else:
+        return None
+
+
+
+    
+
+
+    
+
+
